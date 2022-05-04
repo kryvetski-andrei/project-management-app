c06ab75d15a74a61c3bfccd6f1911f43e1a334e7
kryvetski-andrei <kryvetskistud@gmail.com>
2022-05-05 00:25:57 +0300

    docs: add eslint scripts descriptions README.md

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,7 @@
-import logo from "./logo.svg";
-import style from "./App.module.scss";
+import style from './App.module.scss';
 
 function App() {
-  return (
-    <div className={style.App}>
-      <header className={style["App-header"]}>
-        <img src={logo} className={style["App-logo"]} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className={style["App-link"]}
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <h1 className={style['App-title']}>Project management app</h1>;
 }
 
 export default App;
