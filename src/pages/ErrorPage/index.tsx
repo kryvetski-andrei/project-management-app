85374503eaca8ad700b44fc623f54038fb93cb6e
Katsiaryna Voronich <katarinka403@gmail.com>
2022-05-24 00:21:31 +0300

    feat: add error page component

--- src/pages/ErrorPage/index.tsx
+++ src/pages/ErrorPage/index.tsx
@@ -1,9 +1,21 @@
+import { Stack, Typography } from '@mui/material';
 import React from 'react';
 
 const ErrorPage = () => {
   return (
-    <div>
-      <h2>Error Page</h2>
+    <div
+      style={{
+        height: '100vh',
+        background: '#d1d8d5',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <Stack spacing={4} alignItems="center">
+        <Typography variant="h1">404</Typography>
+        <Typography variant="h2">Oops! This Page Not Found!</Typography>
+      </Stack>
     </div>
   );
 };
