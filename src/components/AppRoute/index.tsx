52995d636c8213e0684e41b49d2474e932f61c66
SavitskiDenis <random.ssd@yandex.by>
2022-05-23 20:47:45 +0300

    refactor: rebase

--- src/components/AppRoute/index.tsx
+++ src/components/AppRoute/index.tsx
@@ -1,18 +1,18 @@
-import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { pagesPath } from '../../utils/config';
 import WelcomePage from '../../pages/WelcomePage';
 import MainPage from '../../pages/MainPage';
 import ErrorPage from '../../pages/ErrorPage';
 import BoardPage from '../../pages/BoardPage';
+import SignUpPage from '../../pages/SignUpPage';
 
 const AppRouter = () => {
   return (
     <Routes>
       <Route path={pagesPath.welcomeUsPagePath} element={<WelcomePage />} />
       <Route path={pagesPath.mainPagePath} element={<MainPage />} />
-      <Route path={pagesPath.boardPagePath} element={<BoardPage />} />
-      <Route path={pagesPath.registrationPagePath} element={<RegistrationPage />} />
+      <Route path={pagesPath.borderPagePath} element={<BoardPage />} />
+      <Route path={pagesPath.signupPagePath} element={<SignUpPage />} />
       <Route path="*" element={<ErrorPage />} />
     </Routes>
   );
