428e5df0765138fa26e2db46b2271f8444d7adf1
SavitskiDenis <random.ssd@yandex.by>
2022-05-23 20:47:45 +0300

    refactor: rebase

--- src/components/AppRoute/index.tsx
+++ src/components/AppRoute/index.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { pagesPath } from '../../utils/config';
 import WelcomePage from '../../pages/WelcomePage';
@@ -12,6 +11,7 @@ const AppRouter = () => {
     <Routes>
       <Route path={pagesPath.welcomeUsPagePath} element={<WelcomePage />} />
       <Route path={pagesPath.mainPagePath} element={<MainPage />} />
+      <Route path={pagesPath.signupPagePath} element={<SignUpPage />} />
       <Route path={pagesPath.boardPagePath} element={<BoardPage />} />
       <Route path={pagesPath.loginPagePath} element={<LogInPage />} />
       <Route path={pagesPath.signupPagePath} element={<SignUpPage />} />
