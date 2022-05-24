c74106a9b5394d9a03dee8a2fc52858e9a39d4e7
SavitskiDenis <random.ssd@yandex.by>
2022-05-25 02:10:30 +0300

    refactor: some edit

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 import Footer from './components/Footer';
 import Header from './components/Header';
-
 function App() {
   return (
     <DndProvider backend={HTML5Backend}>
