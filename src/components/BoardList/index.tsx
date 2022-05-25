7c00f45ec5bb91ab000fd0f5a498311489dda841
Katsiaryna Voronich <katarinka403@gmail.com>
2022-05-26 01:17:40 +0300

    refactor: update  board list component

--- src/components/BoardList/index.tsx
+++ src/components/BoardList/index.tsx
@@ -1,12 +1,13 @@
 import { ArrowRightAlt } from '@mui/icons-material';
-import { Box, IconButton, Paper, Stack, styled } from '@mui/material';
+import { Box, IconButton, Paper, Stack, styled, Typography } from '@mui/material';
 import { Link } from 'react-router-dom';
 import { colorsGenetator } from '../../utils/common/common';
 import ClearIcon from '@mui/icons-material/Clear';
 import classes from './index.module.scss';
-import { BoardListProps } from '../../utils/types/BoardList/index';
+import { useAppDispatch, useAppSelector } from '../../hooks/useAppDispatch';
+import { mainSlice } from '../../store/reducers/mainReducers';
 
-const BoardList = ({ boards, onClick }: BoardListProps) => {
+const BoardList = () => {
   const Board = styled(Paper)(({ theme }) => ({
     boxSizing: 'border-box',
     maxWidth: '1180px',
@@ -20,19 +21,31 @@ const BoardList = ({ boards, onClick }: BoardListProps) => {
     ...theme.typography.h6,
     color: '#000000',
   }));
+  const { boards } = useAppSelector((state) => state.mainReducer);
+  const { setOpenModal, setActiveModal, setIdBoard } = mainSlice.actions;
+  const dispatch = useAppDispatch();
+  const handleClick = (e: React.UIEvent<HTMLButtonElement>) => {
+    dispatch(setOpenModal());
+    dispatch(setActiveModal('delete'));
+    const id = e.currentTarget.id;
+    dispatch(setIdBoard(id));
+  };
   return (
     <Stack spacing={2}>
-      {boards.map(({ id, title }) => (
+      {boards.map(({ id, title, description }) => (
         <Board sx={{ backgroundColor: colorsGenetator() }} key={id}>
           <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
             {title}
-            <IconButton onClick={onClick} id={id}>
+            <IconButton onClick={handleClick} id={id}>
               <ClearIcon />
             </IconButton>
           </Box>
-          <Link to={'/board'} className={classes.link}>
-            <ArrowRightAlt />
-          </Link>
+          <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+            <Typography>{description}</Typography>
+            <Link to={'/board'} className={classes.link}>
+              <ArrowRightAlt />
+            </Link>
+          </Box>
         </Board>
       ))}
     </Stack>
