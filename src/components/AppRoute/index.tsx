6d00f4a1ad960fa26f7db874c820f98c5bb199d8
Katsiaryna Voronich <katarinka403@gmail.com>
2022-05-30 22:47:47 +0300

    feat: add edit profile page

--- src/components/AppRoute/index.tsx
+++ src/components/AppRoute/index.tsx
@@ -6,6 +6,7 @@ import MainPage from '../../pages/MainPage';
 import RegistrationPage from '../../pages/RegistrationPage';
 import ErrorPage from '../../pages/ErrorPage';
 import BoardPage from '../../pages/BoardPage';
+import EditProfilePage from '../../pages/EditProfilePage';
 
 const AppRouter = () => {
   return (
@@ -14,6 +15,7 @@ const AppRouter = () => {
       <Route path={pagesPath.mainPagePath} element={<MainPage />} />
       <Route path={pagesPath.boardPagePath} element={<BoardPage />} />
       <Route path={pagesPath.registrationPagePath} element={<RegistrationPage />} />
+      <Route path={pagesPath.editProfilePagePath} element={<EditProfilePage />} />
       <Route path="*" element={<ErrorPage />} />
     </Routes>
   );
--- src/utils/config.ts
+++ src/utils/config.ts
@@ -4,4 +4,5 @@ export const pagesPath = {
   registrationPagePath: '/registration',
   boardPagePath: '/board',
   errorPagePath: '/error',
+  editProfilePagePath: '/edit',
 };
