ab201368db1d7b0151bbfe5fa26036232a5e0c4e
SavitskiDenis <random.ssd@yandex.by>
2022-05-30 22:38:29 +0300

    feat: modif utils

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -5,6 +5,7 @@ export const pagesPath = {
   signupPagePath: '/signup',
   borderPagePath: '/border',
   errorPagePath: '/error',
+  editProfilePath: '/edit_profile',
 };
 
 export const SERVER_ADDR = 'https://kryvetski-be.herokuapp.com';
--- src/utils/types/authorization/index.ts
+++ src/utils/types/authorization/index.ts
@@ -2,10 +2,12 @@ export const TOKEN_STORAGE_NAME = 'token';
 
 export interface IAuthState {
   token: string | null;
+  userId: string | null;
 }
 
 export enum AuthActionTypes {
   UPDATE_TOKEN = 'UPDATE_TOKEN',
+  UPDATE_USER_ID = 'UPDATE_USER_ID',
 }
 
 interface IActionUpdateToken {
@@ -13,4 +15,9 @@ interface IActionUpdateToken {
   payload: string | null;
 }
 
-export type IAuthdAction = IActionUpdateToken;
+interface IActionUpdateUserId {
+  type: AuthActionTypes.UPDATE_USER_ID;
+  payload: string | null;
+}
+
+export type IAuthdAction = IActionUpdateToken | IActionUpdateUserId;
