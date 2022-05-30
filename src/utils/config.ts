1c97ece050bad5427fbd2d0565b5763aa62c70fa
SavitskiDenis <random.ssd@yandex.by>
2022-05-30 22:38:29 +0300

    feat: modif utils

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -5,6 +5,7 @@ export const pagesPath = {
   signupPagePath: '/signup',
   boardPagePath: '/board',
   errorPagePath: '/error',
+  editProfilePath: '/edit_profile',
 };
 
 export const SERVER_ADDR = 'https://kryvetski-be.herokuapp.com';
---
+++ src/utils/types/languages/index.ts
@@ -0,0 +1,23 @@
+export const LANG_STORAGE_NAME = 'lang';
+
+export interface ILanguageState {
+  current: string;
+}
+
+export enum LanguageActionTypes {
+  SET_CURRENT = 'SET_CURRENT',
+}
+
+export enum LanguageName {
+  ru = 'ru',
+  eng = 'eng',
+}
+
+export const LANGUAGES_LIST = [LanguageName.ru, LanguageName.eng];
+
+interface IActionUpdateSetCurrent {
+  type: LanguageActionTypes.SET_CURRENT;
+  payload: LanguageName;
+}
+
+export type ILanguageAction = IActionUpdateSetCurrent;
