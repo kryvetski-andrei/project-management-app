934efa853a282babca2f816fc3a92c0a0626e82e
SavitskiDenis <random.ssd@yandex.by>
2022-05-30 22:39:35 +0300

    feat: add reducers

--- src/store/reducers/index.ts
+++ src/store/reducers/index.ts
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import { authReducer } from './authReducer';
 import { boardReducer } from './boardReducer';
+import { langReducer } from './languageReducer';
 
 export const rootReducer = combineReducers({
   board: boardReducer,
   auth: authReducer,
+  lang: langReducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
