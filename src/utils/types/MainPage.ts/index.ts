4c992d9770851fc95215a555c7df52704e08c328
Katsiaryna Voronich <katarinka403@gmail.com>
2022-05-30 22:50:01 +0300

    refactor: update types

--- src/utils/types/MainPage.ts/index.ts
+++ src/utils/types/MainPage.ts/index.ts
@@ -1,4 +1,29 @@
 export interface Board {
   id: string;
   title: string;
+  description: string;
+}
+
+export interface MainReducersState {
+  openModal: boolean;
+  idBoard: string;
+  boards: Board[];
+  isLoading: boolean;
+  activeModal: string;
+  status: string | null;
+  error: string | null;
+}
+
+export interface NewBoard {
+  title: string;
+  description: string;
+}
+
+export interface BoardItem extends NewBoard {
+  id: string;
+}
+
+export interface Errors {
+  title?: string;
+  description?: string;
 }
