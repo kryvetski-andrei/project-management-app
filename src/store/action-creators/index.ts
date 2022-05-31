5feed379e357aa1e9f09344639f8721463bfbc1c
Katsiaryna Voronich <katarinka403@gmail.com>
2022-05-31 06:35:23 +0300

    refactor: update files

--- src/store/action-creators/index.ts
+++ src/store/action-creators/index.ts
@@ -1,13 +1,13 @@
 import * as BoardActionCreators from './board';
 import * as ColumnActionCreators from './column';
-import * as BordListActionCreators from './main';
+import * as GetBordActionCreators from './getBoards';
 import * as DeleteBordActionCreators from './deleteBoard';
 import * as SetBordActionCreators from './setBoard';
 
 export default {
   ...BoardActionCreators,
   ...ColumnActionCreators,
-  ...BordListActionCreators,
+  ...GetBordActionCreators,
   ...DeleteBordActionCreators,
   ...SetBordActionCreators,
 };
